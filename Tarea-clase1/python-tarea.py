f7a866121f3ce690869ba93d6476104caf01426e
RenzzoGutierrez20 <u202014777@upc.edu.pe>
2023-03-02 19:42:36 -0600

    Cambio 2

--- Tarea-clase1/python-tarea.py
+++ Tarea-clase1/python-tarea.py
@@ -4,6 +4,7 @@
 #
 
 name = input("Bienvenido, por favor ingrese sus datos: ");
+# Renzo Hubert Gutiérrez Ramos 
 print("Sus datos son: ",name)
 
 
@@ -16,3 +17,13 @@ print("Sus datos son: ",name)
 #    2.Calcule el área de un círculo, triángulo y cuadrado con radio ingresado
 #    desde el teclado
 #
+
+radio = input("Bienvenido, por favor ingrese un valor para el radio - Area de Circulo: ")
+radio = input("Ingrese dos valores para Base y Altura - Area de Triangulo: ")
+pi = 3.14
+area = pow(radio,2)*pi
+print("El valor del área de un circulo es: ", area)
+
+
+
+
