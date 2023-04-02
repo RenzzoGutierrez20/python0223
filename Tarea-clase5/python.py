0a7430881fa603844f52f8f9479c2f5600b72e22
RenzzoGutierrez20 <u202014777@upc.edu.pe>
2023-04-02 08:34:12 -0600

    Refresh

---
+++ Tarea-clase5/python.py
@@ -0,0 +1,9 @@
+import pandas as pd
+df_cliente = pd.read_csv('./Tarea-clase5/input/Datos_csv.csv')
+df_cliente.head()
+
+df_prov = pd.read_csv('./Tarea-clase5/input/DATOS_CVS2.csv')
+df_prov.head()
+
+
+
